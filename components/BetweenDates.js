aab9781d862e6187193b913d2008adb5a723d6a6
TBM85 <tballesterm@gmail.com>
2021-08-12 16:30:16 -0400

    Display difference in days, between selected dates

--- components/BetweenDates.js
+++ components/BetweenDates.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
 
 const BetweenDates = () => {
   const firstInput = useRef();
@@ -20,23 +20,26 @@ const BetweenDates = () => {
 
   // Calculate the difference between the first and second selected date
   const resultInDays =
-  (new Date(secondDate).getTime() - new Date(firstDate).getTime()) /
-  (1000 * 3600 * 24);
-
-  console.log(resultInDays);
+    (new Date(secondDate).getTime() - new Date(firstDate).getTime()) /
+    (1000 * 3600 * 24);
 
   return (
-    <form onSubmit={calculateHandler} className="tab">
-      <div className="form_control">
-        <label htmlFor="first-date">First Date:</label>
-        <input type="date" id="first-date" ref={firstInput} />
+    <Fragment>
+      <form onSubmit={calculateHandler} className="tab">
+        <div className="form_control">
+          <label htmlFor="first-date">First Date:</label>
+          <input type="date" id="first-date" ref={firstInput} />
+        </div>
+        <div className="form_control">
+          <label htmlFor="second-date">Second Date:</label>
+          <input type="date" id="second-date" ref={secondInput} />
+        </div>
+        <button type="submit">Calculate</button>
+      </form>
+      <div>
+        The difference between the selected dates is {resultInDays} days
       </div>
-      <div className="form_control">
-        <label htmlFor="second-date">Second Date:</label>
-        <input type="date" id="second-date" ref={secondInput} />
-      </div>
-      <button type="submit">Calculate</button>
-    </form>
+    </Fragment>
   );
 };
 
