df54ba4c8b3561c34f6d38710f3d880c7b95f203
TBM85 <tballesterm@gmail.com>
2021-08-12 15:47:36 -0400

    The values of the date inputs are obtained

--- components/BetweenDates.js
+++ components/BetweenDates.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const BetweenDates = () => {
+  const firstInput = useRef();
+  const secondInput = useRef();
+
+  const [firstDate, setFirstDate] = useState();
+  const [secondDate, setSecondDate] = useState();
+
+  // Sets the value of date inputs
+  const calculateHandler = (event) => {
+    event.preventDefault();
+
+    const enteredFirstValue = firstInput.current.value;
+    const enteredSecondValue = secondInput.current.value;
+
+    setFirstDate(enteredFirstValue);
+    setSecondDate(enteredSecondValue);
+  };
+
+  console.log(firstDate);
+  console.log(secondDate);
+
   return (
-    <form className="tab">
+    <form onSubmit={calculateHandler} className="tab">
       <div className="form_control">
         <label htmlFor="first-date">First Date:</label>
-        <input type="date" id="first-date" />
+        <input type="date" id="first-date" ref={firstInput} />
       </div>
       <div className="form_control">
         <label htmlFor="second-date">Second Date:</label>
-        <input type="date" id="second-date" />
+        <input type="date" id="second-date" ref={secondInput} />
       </div>
       <button type="submit">Calculate</button>
     </form>
