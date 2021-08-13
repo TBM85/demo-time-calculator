8d4d12becb6cb0f2056f430240501623c87a69a5
TBM85 <tballesterm@gmail.com>
2021-08-13 17:51:57 -0400

    Create a FormControl component

--- components/BetweenDates.js
+++ components/BetweenDates.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
+import FormControl from "./FormControl";
 
 const BetweenDates = () => {
   const [firstDate, setFirstDate] = useState("");
@@ -36,42 +37,39 @@ const BetweenDates = () => {
   // Update the first date value every time it changes
   const firstDateHandler = (event) => {
     setFirstDate(event.target.value);
-  }
+  };
 
   // Update the second date value every time it changes
   const secondDateHandler = (event) => {
     setSecondDate(event.target.value);
-  }
+  };
 
   useEffect(() => {
     if (firstDate.length > 0 && secondDate.length > 0) {
       setIsError(false);
-    };
+    }
   }, [firstDate, secondDate]);
 
   return (
     <Fragment>
       {!isSubmitted ? (
         <form onSubmit={calculateHandler} className="tab">
-          <div className="form_control">
-            <label htmlFor="first-date">First Date:</label>
-            <input
-              type="date"
-              id="first-date"
-              value={firstDate}
-              onChange={firstDateHandler}
-            />
-          </div>
-          <div className="form_control">
-            <label htmlFor="second-date">Second Date:</label>
-            <input
-              type="date"
-              id="second-date"
-              min={firstDate}
-              value={secondDate}
-              onChange={secondDateHandler}
-            />
-          </div>
+          <FormControl
+            id="first-date"
+            htmlFor="first-date"
+            value={firstDate}
+            onChange={firstDateHandler}
+          >
+            First Date:
+          </FormControl>
+          <FormControl
+            id="second-date"
+            htmlFor="second-date"
+            value={secondDate}
+            onChange={secondDateHandler}
+          >
+            Second Date:
+          </FormControl>
           <small className={isError ? "danger" : "transparent"}>
             *A date is required on both entries
           </small>
---
+++ components/FormControl.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FormControl = (props) => {
+  return (
+    <div className="form_control">
+      <label htmlFor={props.htmlFor}>{props.children}</label>
+      <input
+        type="date"
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    </div>
+  );
+};
+
+export default FormControl;
