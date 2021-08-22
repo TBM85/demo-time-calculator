3523cf866641bfac94878a7c36258beba69d1833
TBM85 <tballester.email@gmail.com>
2021-08-22 01:08:05 +0000

    Deploying to gh-pages from @ TBM85/demo-time-calculator@0eab4f90f75054787bc4a9d4f3fcbf75e3eb02d5 🚀

---
+++ _next/static/9O4cz1pfsRIFI-oc_eYPq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/27c1c1486d219d960198.css","static/chunks/pages/index-1db854b542f3cf317044.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
